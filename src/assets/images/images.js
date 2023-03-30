2659ccc5c941758cfc93e13c24f8b67de77313ff
lalit-shendage <shendage.lalit@gmail.com>
2023-03-30 11:44:08 +0530

    first commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./118 - ROG Wallpaper Challenge - 4K (2).jpg";
+import wallpaper2 from "./123 - ROG Wallpaper Challenge - 4K (2).jpg";
+import wallpaper3 from "./130 - ROG Wallpaper Challenge - 4K (2).jpg";
+import wallpaper4 from "./135 - ROG Wallpaper Challenge - 4K.png";
+import wallpaper5 from "./145 - ROG Wallpaper Challenge - 4K (2).jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jfif";
+import allsongs from "./allsongs.gif";
+import artists from "./artist.jpg";
+import albums from "./albums.png";
+import song1Img from "./young_dumb.png";
+import song2Img from "./alone.jfif";
+import song3Img from "./bbt.jpg";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Khalid - Young Dumb.mp3";
+import music2 from "./Marshmello - Alone Monstercat Official Music Video.mp3";
+import music3 from "./The Big Bang Theory - Full.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
